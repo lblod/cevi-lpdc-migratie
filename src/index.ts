cbd11901cf787226ca684b7dd04e245d7e2587ce
pdhondt <44711935+pdhondt@users.noreply.github.com>
2023-10-09 13:19:27 +0200

    added readCeviXml function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
+import {CeviProduct} from "./ceviProduct";
+import {readCeviXml} from "./readCeviXml";
+import {mapToABBProduct} from "./mapToABBProduct";
+async function main() {
 
-function main(message: string) {
-    console.log(message);
+    const timestamp = new Date();
+    const ceviProducts: CeviProduct[] = await readCeviXml('src/resources/LPDC_Stekene.xml');
+    const abbProducts = ceviProducts.map((ceviProduct: CeviProduct) => mapToABBProduct(ceviProduct, timestamp, 'Stekene'));
+    // abbProduct.toTriples();
+
+
+
+    console.log(ceviProducts[1]);
+    console.log(ceviProducts[1].deliveringDepartments[0].address);
 }
 
-main('Hello world!');
+main();
---
+++ src/readCeviXml.ts
@@ -0,0 +1,16 @@
+import fs from "fs/promises";
+import convert from "xml-js";
+import {CeviProduct} from "./ceviProduct";
+import {mapXmlToCeviProduct} from "./mapXmlToCeviProduct";
+
+export async function readCeviXml(pathToFile: string): Promise<CeviProduct[]> {
+    const xml = await fs.readFile(pathToFile, { encoding: "utf-8"});
+    const options = {compact: true};
+    const result = convert.xml2json(xml, options); // TODO: check of xml2js gives same result
+    // await fs.writeFile('xml-to-json-result-compact.json', result);
+    const parsedResult = JSON.parse(result);
+
+    const products = parsedResult.string.ProductCatalog.Products.Product;
+
+    return products.map((product: any) => mapXmlToCeviProduct(product));
+}
