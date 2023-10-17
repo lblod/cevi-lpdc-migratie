f05d1883e5177956b0174c0e1cd9b5bcb8969a9e
gauquiebart <bart.gauquie@gmail.com>
2023-10-17 11:49:10 +0200

    LPDC-718: add language to migration process + log errors + reuse script for migrate-stekene as well

--- src/migrate-cevi.ts
+++ src/migrate-cevi.ts
@@ -1,12 +1,14 @@
 import {runProcess} from "./migration-process";
+import {Language} from "./language";
 
 async function main() {
 
     const xmlFileName =  'src/resources/LPDC_CEVI.xml';
     const bestuurseenheidUuid = '6025a5d1ca2262a784f002edd8ce9ca9073ae3d5ebc6b6b5531f05a29e9250af';
     const lokaalBestuurNis2019Url = 'http://vocab.belgif.be/auth/refnis2019/46024';
+    const language = Language.INFORMAL;
 
-    await runProcess(xmlFileName, bestuurseenheidUuid, lokaalBestuurNis2019Url);
+    await runProcess(xmlFileName, bestuurseenheidUuid, lokaalBestuurNis2019Url, language);
 }
 
 main();
--- src/migration-process.ts
+++ src/migration-process.ts
@@ -6,7 +6,7 @@ import {mapToABBProduct} from "./mapToABBProduct";
 import {Language} from "./language";
 import fsp from "fs/promises";
 
-export async function runProcess(xmlFileName: string, bestuurseenheidUuid: string, lokaalBestuurNis2019Url: string) {
+export async function runProcess(xmlFileName: string, bestuurseenheidUuid: string, lokaalBestuurNis2019Url: string, language: Language) {
 
     if(!fs.existsSync(xmlFileName)) {
         console.error(`"${xmlFileName}" not found ... stopping`);
@@ -18,29 +18,37 @@ export async function runProcess(xmlFileName: string, bestuurseenheidUuid: strin
     const timestamp = new Date();
     const logStream = fs.createWriteStream(`src/migration-results/${baseName}-${timestamp.toISOString()}-migration.log`);
 
-    console.log = function(message?: any, ...optionalParams: any[]) {
-        logStream.write(`${message}\n`);
+    console.log = function(message?: any, exception?: Error) {
+        let logMessage = message;
+        if (exception) {
+            logMessage += ' Exception: ' + exception.stack;
+        }
+        logStream.write(`${logMessage}\n`);
     };
 
-    console.error = function(message?: any, ...optionalParams: any[]) {
-        logStream.write(`    ${message}\n`);
+    console.error = function(message?: any, exception?: Error) {
+        let errorMessage = message;
+        if (exception) {
+            errorMessage += ' Exception: ' + exception.stack;
+        }
+        logStream.write(`    ${errorMessage}\n`);
     };
 
     const ceviProducts: CeviProduct[] = await readCeviXml(xmlFileName);
     fs.copyFileSync(xmlFileName, `src/migration-results/${baseName}-${timestamp.toISOString()}.xml`)
     const abbProducts = ceviProducts.map((ceviProduct: CeviProduct): AbbProduct | undefined => {
         try {
-            console.log(`\n--- Mapping cevi Product ${ceviProduct.id} ---`);
+            console.log(`\n--- Mapping cevi Product ${ceviProduct.id || '(none specified)'} ---`);
             return mapToABBProduct(ceviProduct, timestamp, `http://data.lblod.info/id/bestuurseenheden/${bestuurseenheidUuid}`, lokaalBestuurNis2019Url);
         }
         catch (error) {
-            console.error(`Cevi product ${ceviProduct.id} with name ${ceviProduct.name} could not be mapped to an ABB Product at ${timestamp}`, error);
+            console.error(`Cevi product ${ceviProduct.id} with name ${ceviProduct.name} could not be mapped to an ABB Product.`, error);
             return undefined;
         }
     });
 
-    const triples = abbProducts[0]?.toTriples(Language.INFORMAL).map(trip => trip.toString()).join('\n');
-    triples ? await fsp.writeFile(`src/migration-results/${baseName}-${timestamp.toISOString()}-migration.ttl`, triples) : console.error(`No triples to write to .ttl file`);
+    const triples = abbProducts.flatMap(abbProduct => abbProduct?.toTriples(language)).map(trip => trip?.toString()).join('\n');
+    await fsp.writeFile(`src/migration-results/${baseName}-${timestamp.toISOString()}-migration.ttl`, triples);
     await fsp.writeFile(`src/migration-results/${baseName}-${timestamp.toISOString()}-migration.graph`, `http://mu.semte.ch/graphs/organizations/${bestuurseenheidUuid}/LoketLB-LPDCGebruiker`);
 
 }
